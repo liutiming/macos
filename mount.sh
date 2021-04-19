e71931fdcd8384b5254abc773c3637e5890b52e8
Timing Liu <39947942+liutiming@users.noreply.github.com>
2021-04-19 15:58:07 +0100

    add VPN

--- mount.sh
+++ mount.sh
@@ -3,3 +3,4 @@ sshfs -o defer_permissions farm5-login:/lustre/scratch123/hgi/teams/martin/tl11/
 sshfs -o defer_permissions farm5-login:/lustre/scratch119/humgen/teams/martin/users/tl11/ ~/Mounts/AnalysisDir -o volname="analysis" 
 
 # to umount, umount _server-location_ : https://phpsolved.com/mount-point-xyz-osxfuse-volume/
+# cannot be automated on startup since it requires VPN
