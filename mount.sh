a0c30a448e831e3e74c11704a0fcbc4b9eb1af15
Timing Liu <39947942+liutiming@users.noreply.github.com>
2021-04-19 15:55:43 +0100

    first commit

---
+++ mount.sh
@@ -0,0 +1,3 @@
+sshfs -o defer_permissions farm5-login:/nfs/users/nfs_t/tl11 ~/Mounts/HomeDir -o volname="tl11"
+sshfs -o defer_permissions farm5-login:/lustre/scratch123/hgi/teams/martin/tl11/ ~/Mounts/Analysis_123 -o volname="analysis_123"
+sshfs -o defer_permissions farm5-login:/lustre/scratch119/humgen/teams/martin/users/tl11/ ~/Mounts/AnalysisDir -o volname="analysis" 
